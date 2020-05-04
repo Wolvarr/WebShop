78b14cb1599aa093b1161ff77cad195f2b3e13bf
Richárd Pasics <43377943+Wolvarr@users.noreply.github.com>
2020-05-04 15:57:24 +0200

    frontend yo

---
+++ WebShop.Mvc/Controllers/PcBuilderController.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace WebShop.Mvc.Controllers
+{
+    public class PcBuilderController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
